bc63bdd65cd4f0938547ef0c61ec9446477a5362
salgad6 <salgad6@stolaf.edu>
2026-05-14 16:41:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -68,26 +68,13 @@ function checkCookie() {
 function CookiePetCREATE() {
   //let PETNAME = getCookie("username")
   //make the user give it a name
-  const now = new Date();
-  const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
-
-  const utcString = tomorrow.toUTCString();
   cookieContainer.style.display = 'block';
   const randomIndex = Math.floor(Math.random() * imageLink.length);
   displayImg.src = imageLink[randomIndex];
-  document.cookie = "VISITED:TRUE"; path=/; expires=${utcString};
+  document.cookie = "cookie:CREATED";
 }
 
-// Function to check if a specific cookie exists
-function checkCookie(name) {
-  const cookies = document.cookie.split(';');
-  return cookies.some(cookie => cookie.trim().startsWith(name + '='));
-}
 
-// If the cookie 'visited' exists, show the hidden div
-if (checkCookie('VISITED')) {
-  document.getElementById('welcome-message').style.display = 'block';
-}
 
 
 const imageLink = ['chocolate.png', 'oatmeal.png', 'peanutbutter.png'];
