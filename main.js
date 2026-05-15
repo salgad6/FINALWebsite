4b7470f40a72ef5869009188b2f92381e70aec26
salgad6 <salgad6@stolaf.edu>
2026-05-14 19:17:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -52,6 +52,10 @@ function checkCookie() {
     displayImg.src = COOKIE;
     //new line test
   }
+  else {
+    if (on_index != true) {
+      displayImg.src = grave.png;
+  }
 }
 
 function CookiePetCREATE() {
